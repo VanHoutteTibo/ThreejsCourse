0d10cc9bbf851321462e84815fddff37fbbaba44
TiboVanHoutte <tibo.vanhoutte@lvdgroup.com>
2023-08-18 17:20:35 +0200

    finished lesson 16

--- 16-shadows/src/script.js
+++ 16-shadows/src/script.js
@@ -2,6 +2,10 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 
+const textureLoader = new THREE.TextureLoader()
+const bakedShadow = textureLoader.load('/textures/bakedShadow.jpg')
+const simpleShadow = textureLoader.load('/textures/simpleShadow.jpg')
+
 THREE.ColorManagement.enabled = false
 
 /**
@@ -68,10 +72,15 @@ scene.add(spotLightCameraHelper)
 
 const pointLight = new THREE.PointLight(0xffffff, 0.3)
 pointLight.castShadow = true
+pointLight.shadow.mapSize.width = 1024
+pointLight.shadow.mapSize.height = 1024
+pointLight.shadow.camera.near = 0.1
+pointLight.shadow.camera.near = 5
 pointLight.position.set(-1,1,0)
 scene.add(pointLight)
 
 const pointLightCameraHelper = new THREE.CameraHelper(pointLight.shadow.camera)
+pointLightCameraHelper.visible = false
 scene.add(pointLightCameraHelper)
 
 /**
@@ -94,6 +103,7 @@ sphere.castShadow = true
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(5, 5),
     material
+    
 )
 plane.rotation.x = - Math.PI * 0.5
 plane.position.y = - 0.5
@@ -101,6 +111,19 @@ plane.position.y = - 0.5
 plane.receiveShadow = true
 
 scene.add(sphere, plane)
+
+const sphereShadow = new THREE.Mesh(
+    new THREE.PlaneGeometry(1.5, 1.5),
+    new THREE.MeshBasicMaterial({
+        color: 0x000000,
+        transparent: true,
+        alphaMap: simpleShadow
+    })
+)
+
+sphereShadow.rotation.x = -Math.PI * 0.5
+sphereShadow.position.y = plane.position.y + 0.01
+scene.add(sphereShadow)
 
 /**
  * Sizes
@@ -149,7 +172,7 @@ renderer.outputColorSpace = THREE.LinearSRGBColorSpace
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-renderer.shadowMap.enabled = true
+renderer.shadowMap.enabled = false
 renderer.shadowMap.type = THREE.PCFShadowMap
 
 /**
@@ -160,6 +183,15 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update Sphere
+    sphere.position.x = Math.cos(elapsedTime) * 1.5
+    sphere.position.z = Math.sin(elapsedTime) * 1.5
+    sphere.position.y = Math.abs(Math.sin(elapsedTime * 3))
+
+    sphereShadow.position.x = sphere.position.x
+    sphereShadow.position.z = sphere.position.z
+    sphereShadow.material.opacity = (1- sphere.position.y) * 0.3
 
     // Update controls
     controls.update()

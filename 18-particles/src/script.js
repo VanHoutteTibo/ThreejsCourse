4a1290e38d1721d8587e709ff37103b5f78fce68
TiboVanHoutte <tibo.vanhoutte@lvdgroup.com>
2023-09-14 16:33:32 +0200

    18-particles

--- 18-particles/src/script.js
+++ 18-particles/src/script.js
@@ -30,26 +30,33 @@ const particlesGeometry = new THREE.BufferGeometry()
 const count = 5000
 
 const position = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
 
 for(let i = 0; i < count * 3; i++)
 {
     position[i] = (Math.random() - 0.5) * 10
+    colors[i] = Math.random()
 }
 
 particlesGeometry.setAttribute(
     "position",
     new THREE.BufferAttribute(position, 3)
 )
+particlesGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3))
 
 //material
 const particlesMaterial = new THREE.PointsMaterial({
     size: 0.1,
-    sizeAttenuation: true,
-    color: '#ff88cc'
+    sizeAttenuation: true
 })
 
 particlesMaterial.transparent = true
 particlesMaterial.alphaMap = particleTexture
+//particlesMaterial.alphaTest = 0.001
+//particlesMaterial.depthTest = false
+particlesMaterial.depthWrite = false;
+particlesMaterial.blending = THREE.AdditiveBlending
+particlesMaterial.vertexColors = true
 
 //Points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial)
@@ -109,6 +116,17 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    //Update Particles
+    //particles.rotation.y = elapsedTime * 0.25
+
+    for(let i = 0; i < count; i++)
+    {
+        const i3 = i * 3
+        const x = particlesGeometry.attributes.position.array[i3]
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+    }
+    particlesGeometry.attributes.position.needsUpdate = true
 
     // Update controls
     controls.update()
